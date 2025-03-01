fdea1ab555a07f054f63b70eddca48d622cc1dc5
21miusie <mmengesha27@gmail.com>
2025-03-01 18:37:15 +0300

    steps skeleton and UI styles

--- rapp_uno_kepper/public/reactSteps/App.jsx
+++ rapp_uno_kepper/public/reactSteps/App.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="steps">App</div>
+    <div className="steps">
+    
+    <div className='numbers'>
+<div>1</div>
+<div>2</div>
+<div>3</div>
+    </div>
+
+    <p className='message'>Hello</p>
+
+    <div className='buttons'>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Previous</button>
+        <button>Next</button>
+    </div>
+    </div>
   )
 }
 

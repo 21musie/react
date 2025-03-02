6c74ace6f4e4ea980e0319e56f59a97ccea4bd25
21miusie <mmengesha27@gmail.com>
2025-03-02 16:58:35 +0300

    useState hook for handling next and previous steps

--- rapp_uno_kepper/public/reactSteps/App.jsx
+++ rapp_uno_kepper/public/reactSteps/App.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const messages = [ 
+    "Learn React",
+    "Apply for jobs",
+    "Invest your new income"
+];
+
+
+
 
 function App() {
+    const [step, setStep] = useState(1);
+
+    function handlePrevious(){
+    
+    step>1 && setStep(step - 1);
+    }
+    
+    function handleNext(){
+    
+    step<3 && setStep(step + 1);
+    }
+
   return (
     <div className="steps">
     
     <div className='numbers'>
-<div>1</div>
-<div>2</div>
-<div>3</div>
+<div className={step>=1 ? "active" : ""}>1</div>
+<div className={step>=2 ? "active" : ""}>2</div>
+<div className={step>=3 ? "active" : ""}>3</div>
     </div>
 
-    <p className='message'>Hello</p>
+    <p className='message'>
+    Step {step}: {message[step-1]} </p>
 
     <div className='buttons'>
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}}>Previous</button>
-        <button>Next</button>
+        <button onClick="handlePrevious" style={{backgroundColor:'#7950f2', color:'#fff'}}>Previous</button>
+        <button onClick='handleNext' style={{backgroundColor:'#5EBB1E', color:'#fff'}}>Next</button>
     </div>
     </div>
   )

7a037500de058f3d0f68369a017b363eefaa4389
21miusie <mmengesha27@gmail.com>
2025-03-08 23:31:47 +0300

    decorate item component

--- rapp_uno_kepper/public/travelList/Components/Item.jsx
+++ rapp_uno_kepper/public/travelList/Components/Item.jsx
@@ -1,16 +1,21 @@
+// Item component represents a single item in the packing list
 export default function Item({ item, onDeleteItem, onToggleItem }) {
-    return (
-      <li>
-        <input
-          type="checkbox"
-          value={item.packed}
-          onChange={() => onToggleItem(item.id)}
-        />
-        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-          {item.quantity} {item.description}
-        </span>
-        <button onClick={() => onDeleteItem(item.id)}>❌</button>
-      </li>
-    );
-  }
-  
+  return (
+    <li>
+      {/* Checkbox to mark the item as packed or unpacked */}
+      <input
+        type="checkbox"
+        value={item.packed} // Checkbox reflects the packed status
+        onChange={() => onToggleItem(item.id)} // Calls the function to toggle packed status
+      />
+
+      {/* Display item description with strikethrough if packed */}
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+
+      {/* Button to remove the item from the list */}
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
